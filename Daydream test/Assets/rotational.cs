2142ba059a0c35cb5d6ded5b1d6105eed9ddc45f
Zekun Wei <zwei13@illinois.edu>
2017-06-28 11:11:19 -0500

    daydream test

---
+++ Daydream test/Assets/rotational.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotational : MonoBehaviour {
+	public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(Time.deltaTime*10,0,0));
+		transform.RotateAround (camera.transform.position, camera.transform.up, 10 * Time.deltaTime);
+	}
+}
